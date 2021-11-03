4e0b22eb27c614f63e4af683ff2ed172c2894ddc
Benjamin Lewis <23inhouse@gmail.com>
2021-11-03 13:48:10 +0100

    Add boilerplate code

---
+++ TabViewBadges/TabViewBadges/TabViewBadgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewBadgesApp.swift
+//  TabViewBadges
+//
+//  Created by Benjamin Lewis on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TabViewBadgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
